3fd810c2ad6d40a783f1b53196472ffb93bfcdfc
nemaakash692-ctrl <nemaakash692@gmail.com>
2026-08-06 14:01:37 +0530

    Update bot activity name and type

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,9 +23,9 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
+        name: "GTA-6 ", // required by Discord API, not shown in the client
         state: "Strom Own This Bot",     // this is what people actually see
-        type: 0,               // Custom
+        type: 1,               // Custom
       },
     ],
   },
